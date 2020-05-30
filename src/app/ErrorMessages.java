dbb80836b6110678416ae9194ddf3aea19058c20
Agustin Clemente <agustin.clemente@avature.net>
2020-05-30 13:53:01 -0300

    Improve error message

--- src/app/ErrorMessages.java
+++ src/app/ErrorMessages.java
@@ -6,7 +6,7 @@ public class ErrorMessages {
     public static final String ANSI_RESET = "\u001B[0m";
 
     public void existentVariable(String name, int line) {
-        System.out.println(ANSI_RED + "ERROR Line " + line+ ": The variable '" + name + "' was already declared." + ANSI_RESET);
+        System.out.println(ANSI_RED + "ERROR Line " + line+ ": The variable '" + name + "' was already declared in this context." + ANSI_RESET);
     }
 
     public void unexistentVariable(String name, int line) {
