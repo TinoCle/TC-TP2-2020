1831e0d82b5a05c4401b28425a156cf975ba1ce6
Gonzalo <gonzagrisa@gmail.com>
2020-07-19 20:44:08 -0300

    Visitor working with assignantion and variable declaration

--- src/app/App.java
+++ src/app/App.java
@@ -20,7 +20,7 @@ public class App {
         // create a parser that feeds off the tokens buffer
         alParser parser = new alParser(tokens);
         
-        Listener listener = new Listener(parser);
+        CustomListener listener = new CustomListener(parser);
 
         parser.addParseListener(listener);
         // Solicito al parser que comience indicando una regla gramatical
--- src/app/Visitor.java
+++ src/app/Visitor.java
@@ -19,10 +19,9 @@ public class Visitor extends alBaseVisitor<String> {
     private String currentTemp;
 
     public Visitor(){
-        this.code = "";
         this.LblCount = 0;
         this.TmpCount = 0;
-
+        this.code = "";
         this.previousTemp = "";
         this.currentTemp = "";
     }
@@ -44,11 +43,24 @@ public class Visitor extends alBaseVisitor<String> {
     }
 
     @Override
+    public String visitAsignacion(AsignacionContext ctx) {
+        List<ParseTree> factores = getRuleNodes(ctx, alParser.RULE_factor);
+        if (factores.size() <= 2){
+            this.code += ctx.ID().getText() + " = " + get2Factors(factores);
+        } else{
+            OpalContext opalCtx = ctx.asign().operacion().opal();
+            processConjuncion(opalCtx);
+            code += ctx.ID().getText() + " = t" + (TmpCount - 1) + "\n";
+        }
+        return null;
+    }
+
+    @Override
     public String visitDeclaracion(DeclaracionContext ctx) {
         if (ctx.asignacion() != null) {
-            List<ParseTree> ruleFactors = getRuleNodes(ctx, alParser.RULE_factor);
-            if (ruleFactors.size() <= 2) { // just a simple operation of two factors
-                code += ctx.asignacion().ID().getText() + " = " + process2Terms(ruleFactors);
+            List<ParseTree> factores = getRuleNodes(ctx, alParser.RULE_factor);
+            if (factores.size() <= 2){
+                this.code += ctx.asignacion().ID().getText() + " = " + get2Factors(factores);
             } else{
                 OpalContext opalCtx = ctx.asignacion().asign().operacion().opal();
                 processConjuncion(opalCtx);
@@ -101,42 +113,32 @@ public class Visitor extends alBaseVisitor<String> {
         }
     }
 
-    private void concatTemps(String operation) {
-        this.code += String.format("t%d = %s %s %s \n", TmpCount, previousTemp, operation, currentTemp);
-        currentTemp = "t" + TmpCount;
-        TmpCount++;
-    }
-
     private void processTerms(ExpresionContext ctx) {
         List<ParseTree> ruleTerms = new ArrayList<ParseTree>();
-        findRuleNodes(ctx, alParser.RULE_term, ruleTerms);
-        String temp;
+        findRuleNodes(ctx, alParser.RULE_termino, ruleTerms);
+        String temp;        
+
         List<ParseTree> terms = new ArrayList<ParseTree>(ruleTerms);
         for (int i = 0; i < terms.size(); i++) {
             // Lista de factores de ese termino 'i' 9 * 8 / 2 -> [9,8,2]
             List<ParseTree> factors = new ArrayList<ParseTree>();
             findRuleNodes(terms.get(i), alParser.RULE_factor, factors);
-
             // Si tiene mas de un factor -> 9 * 8 / 2
             if (factors.size() > 1) {
                 temp = currentTemp;
                 generateTempsInTerm(factors); // Genero los temporales
-                // t0 = 9 * 8
-                // t1 = t0 / 2
-
                 previousTemp = temp; // almaceno en un auxiliar el temporal actual
-
                 currentTemp = "t" + (TmpCount - 1);
-            } /* else {
+            } else {
                 previousTemp = currentTemp; // almaceno en un auxiliar el temporal actual
-                if (((TermContext) terms.get(i)).factor().opal() != null) {
+                if (((TerminoContext) terms.get(i)).factor().opal() != null) {
                     temp = currentTemp;
-                    processConjuncion(((TermContext) terms.get(i)).factor().opal());
+                    processConjuncion(((TerminoContext) terms.get(i)).factor().opal());
                     previousTemp = temp;
                 } else {
                     currentTemp = factors.get(0).getText(); // el actual es el primero de la lista 9 -> 9 + 1
                 }
-            } */
+            }
             if(i > 0){ 
                 concatTemps(terms.get(i).getParent().getChild(0).getText());
             }
@@ -178,53 +180,50 @@ public class Visitor extends alBaseVisitor<String> {
         }
     }
 
-    public void getRuleNodesWithoutOpal(ParseTree t, int index, List<ParseTree> nodes) {
-        if (t instanceof ParserRuleContext) {
-            ParserRuleContext ctx = (ParserRuleContext) t;
-            if (ctx.getRuleIndex() == index) {
-                nodes.add(t);
-            }
-        }
-        // check children
-        for (int i = 0; i < t.getChildCount(); i++) {
-            if (!(t.getChild(i) instanceof OpalContext)) {
-                getRuleNodesWithoutOpal(t.getChild(i), index, nodes);
-            }
-        }
-    }
-
-    private String process2Terms(List<ParseTree> terms){
+    private String get2Factors(List<ParseTree> terms){
         String code = "";
         for (ParseTree parseTree : terms) {
             FactorContext Fctx = ((FactorContext)parseTree);
-            if (Fctx.getParent().getParent() instanceof alParser.ExpContext){ // if there are terms like 1 + 2
+            if (Fctx.getParent().getParent() instanceof alParser.ExpContext){ // if there's a + or -
                 ExpContext exp = (ExpContext) Fctx.getParent().getParent();
                 String operator = exp.getChild(0).getText();
                 String factor = exp.getChild(1).getText();
                 code += operator + " " +  factor + "\n";
-            } else if (Fctx.getParent() instanceof alParser.TermContext){ //if there's a expression like 1 * 2
+            } else if (Fctx.getParent() instanceof alParser.TermContext){ //if there's a * or /
                 TermContext term = (TermContext) Fctx.getParent();
                 String operator = term.getChild(0).getText();
                 String factor = term.getChild(1).getText();
                 code += operator + " " + factor + "\n";
-            } else { //it's just a factor
+            } else { //it's the first factor, so there's nothing on its left
                 String factor = Fctx.getText();
-                code += factor + (terms.size() == 1 ? "\n" : " ");;
+                code += factor;
+                if (terms.size() == 1){ //last one
+                    code += "\n";
+                }
             }
         }
         return code;
     }
     
-    /* @Override
-    public String visitCondicional(CondicionalContext ctx) {
-        for (int i = 0; i < ctx.operacion().size(); i++) {
-            processConjuncion(ctx.operacion().get(i).opal());
+    private void concatTemps(String operation) {
+        this.code += String.format("t%d = %s %s %s \n", TmpCount, previousTemp, operation, currentTemp);
+        currentTemp = "t" + TmpCount;
+        TmpCount++;
+    }
+
+    @Override
+    public String visitIteracion(IteracionContext ctx) {
+        if (ctx.FOR() != null){
+            System.out.println("MODO GARLOPA");
+        }
+        if (ctx.WHILE() != null){
+            
         }
         return null;
-    } */
-
+    }
 
     public void printCode(){
+        System.out.println("\n=== INTERMEDIATE CODE ===");
         System.out.println(this.code);
     }
 
